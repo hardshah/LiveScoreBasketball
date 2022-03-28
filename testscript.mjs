cdcb1095b9b2f03b4194fe6561e11503e6306268
hard shah <hardshah3@gmail.com>
2022-03-27 18:57:35 -0600

    script to access and serialise data from api

---
+++ testscript.mjs
@@ -0,0 +1,64 @@
+import fetch from 'node-fetch';
+import moment from "moment";
+
+class game{
+    constructor(time,homeTeam,visTeam,homeScore,visScore,period){
+        this.time = time;
+        this.homeTeam = homeTeam;
+        this.visTeam = visTeam;
+        this.homeScore = homeScore;
+        this.visScore = visScore;
+        this.period = period;
+    }
+
+    show(){
+        console.log( this.time ,
+            this.homeTeam,
+            this.visTeam ,
+            this.homeScore ,
+            this.visScore ,
+            this.period )
+    }
+}
+
+async function fetchData(){
+    const today = moment();
+    const tomorrow  = moment().add(1,'days');
+
+    const startDate = today.format('YYYY-MM-DD')+"T06:00:00.000Z";
+    const endDate=tomorrow.format('YYYY-MM-DD')+"T06:00:00.000Z";
+
+    var response = await fetch('https://balldontlie.io/api/v1/games?start_date='+startDate+'&end_date='+endDate);
+    var games= await response.json();
+    await games!=Promise;
+    var GameList = [];
+    for(var i =0; i< games["data"].length;i++){
+
+        let Game = new game(
+            games["data"][i]["status"],
+            games["data"][i]["home_team"]["full_name"],
+            games["data"][i]["visitor_team"]["full_name"],
+            games["data"][i]["home_team_score"],
+            games["data"][i]["visitor_team_score"],
+            games["data"][i]["period"]
+
+
+        )
+
+        
+
+        GameList.push(Game);
+
+    }
+    return GameList;
+
+    
+
+}
+
+var gamelist = await fetchData();
+console.log(gamelist)
+
+
+
+
